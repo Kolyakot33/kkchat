4668ba0cbfdb6f8111a2dceaff3fb2ec86c21849
Kolyakot33 <Kolyakot33@gmail.com>
2021-07-23 11:38:44 +0300

    initial commit

---
+++ src/ru/kolyakot33/kkchat/Main.java
@@ -0,0 +1,35 @@
+package ru.kolyakot33.kkchat;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
+
+
+public class Main extends JavaPlugin {
+    FileConfiguration configuration = getConfig();
+
+    public int range;
+    public String globalSymbol;
+    public String globalFormat;
+    public String localFormat;
+    private static Main INSTANCE;
+
+    @Override
+    public void onEnable() {
+        INSTANCE = this;
+        saveDefaultConfig();
+        range = configuration.getInt("local-chat-range");
+        globalSymbol = configuration.getString("global-chat-symbol");
+        globalFormat = configuration.getString("global-chat-format");
+        localFormat = configuration.getString("local-chat-format");
+        getLogger().log(Level.INFO , "Configuration loaded!");
+        getServer().getPluginManager().registerEvents(new EventsListener(), this);
+        getLogger().log(Level.INFO , "Events registered!");
+        setEnabled(true);
+        getLogger().log(Level.INFO , "Loaded!");
+    }
+    public static Main getInstance() {
+        return INSTANCE;
+    }
+}
